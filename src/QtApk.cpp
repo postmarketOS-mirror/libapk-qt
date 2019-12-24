42f48a5c6a70ff3def4836d90db0091c89b99462
Alexey Min <alexey.min@gmail.com>
2019-12-24 19:57:01 +0300

    QtApk: open/close methods and link to Qt5::Core

--- src/QtApk.cpp
+++ src/QtApk.cpp
@@ -1,7 +1,76 @@
 #include "QtApk.h"
 
+extern "C" {
+#include "apk_blob.h"
+#include "apk_database.h"
+#include "apk_defines.h"
+#include "apk_version.h"
+}
+
 namespace QtApk {
 
+
+class DatabasePrivate
+{
+public:
+    DatabasePrivate(Database *q)
+        : q_ptr(q)
+    {
+        //
+    }
+
+    bool open() {
+        memset(&opts, 0, sizeof(opts));
+        opts.open_flags = APK_OPENF_READ | APK_OPENF_WRITE | APK_OPENF_CREATE | APK_OPENF_NO_AUTOUPDATE;
+        list_init(&opts.repository_list);
+        apk_atom_init();
+        db = static_cast<struct apk_database *>(malloc(sizeof(struct apk_database)));
+        apk_db_init(db);
+        int r = apk_db_open(db, &opts);
+        if (r != 0) {
+            free(db);
+            db = nullptr;
+            return false;
+        }
+        return true;
+    }
+
+    void close() {
+        if (db) {
+            apk_db_close(db);
+            free(db);
+            db = nullptr;
+        }
+        return;
+    }
+
+    Database *q_ptr = nullptr;
+    Q_DECLARE_PUBLIC(Database)
+
+    struct apk_db_options opts;
+    struct apk_database *db = nullptr;
+};
+
+///////////////////////////////////////////////////////////////////
+///////////////////////////////////////////////////////////////////
+
+Database::Database()
+    : d_ptr(new DatabasePrivate(this))
+{
+    //
+}
+
+bool Database::open()
+{
+    Q_D(Database);
+    return d->open();
+}
+
+void Database::close()
+{
+    Q_D(Database);
+    d->close();
+}
 
 
 } // namespace QtApk
--- src/QtApk.h
+++ src/QtApk.h
@@ -1,7 +1,20 @@
+#include <QObject>
+
 namespace QtApk {
+
+class DatabasePrivate;
 
 class Database {
 public:
+    Database();
+
+public:
+    bool open();
+    void close();
+
+private:
+    DatabasePrivate *d_ptr = nullptr;
+    Q_DECLARE_PRIVATE(Database)
 };
 
 } // namespace QtApk
