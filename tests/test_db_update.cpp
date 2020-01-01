814fe1730fa78f3a8d710f99c2f89b04a938dbfd
Alexey Min <alexey.min@gmail.com>
2020-01-01 16:23:46 +0300

    tests: update to new API usage

--- tests/test_db_update.cpp
+++ tests/test_db_update.cpp
@@ -8,13 +8,13 @@ int main()
     int ret = 0;
     QtApk::Database db;
 
-    if (!db.open()) {
+    if (!db.open(QtApk::Database::QTAPK_OPENF_READWRITE)) {
         qWarning() << "Failed to open APK DB!";
         return 1;
     }
     qDebug() << "OK: DB was opened!";
 
-    if (!db.update(true)) {
+    if (!db.updatePackageIndex(true)) {
         qWarning() << "WARNING: Failed to update DB!";
     } else {
         qDebug() << "OK: DB was updated!";
--- tests/test_openclose.cpp
+++ tests/test_openclose.cpp
@@ -8,11 +8,11 @@ int main()
     int ret = 0;
     QtApk::Database db;
 
-    if (!db.open()) {
+    if (!db.open(QtApk::Database::QTAPK_OPENF_READONLY)) {
         qWarning() << "Failed to open APK DB!";
         return 1;
     }
-    qDebug() << "OK: DB was opened/created";
+    qDebug() << "OK: DB was opened.";
 
     db.print_installed();
 
