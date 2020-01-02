b5092c807569f321d7de1dd95970a9470b57c680
Alexey Min <alexey.min@gmail.com>
2020-01-02 03:34:29 +0300

    tests: script: operative relatively to script directory

--- tests/testdata/create_fakeroot.sh
+++ tests/testdata/create_fakeroot.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
 ROOT=$1
+SCRIPT_DIR=$(realpath $(dirname $0) )
 
 if [ x$ROOT == "x" ]; then
     echo "Usage: $0 FAKEROOT_DIR"
@@ -16,16 +17,16 @@ mkdir -p $ROOT/lib/apk/db
 mkdir -p $ROOT/var/cache/apk
 mkdir -p $ROOT/var/cache/misc
 
-cp -a files/*.pub         $ROOT/etc/apk/keys/
+cp -a $SCRIPT_DIR/files/*.pub         $ROOT/etc/apk/keys/
 
-cp -a files/arch          $ROOT/etc/apk/
-cp -a files/repositories  $ROOT/etc/apk/
-cp -a files/world         $ROOT/etc/apk/
+cp -a $SCRIPT_DIR/files/arch          $ROOT/etc/apk/
+cp -a $SCRIPT_DIR/files/repositories  $ROOT/etc/apk/
+cp -a $SCRIPT_DIR/files/world         $ROOT/etc/apk/
 
-cp -a files/installed     $ROOT/lib/apk/db
-cp -a files/lock          $ROOT/lib/apk/db
-cp -a files/scripts.tar   $ROOT/lib/apk/db
-cp -a files/triggers      $ROOT/lib/apk/db
+cp -a $SCRIPT_DIR/files/installed     $ROOT/lib/apk/db
+cp -a $SCRIPT_DIR/files/lock          $ROOT/lib/apk/db
+cp -a $SCRIPT_DIR/files/scripts.tar   $ROOT/lib/apk/db
+cp -a $SCRIPT_DIR/files/triggers      $ROOT/lib/apk/db
 
 # APKINDEX cache should match repositories file
-cp -aR files/APKINDEX.*.tar.gz  $ROOT/var/cache/apk
+cp -aR $SCRIPT_DIR/files/APKINDEX.*.tar.gz  $ROOT/var/cache/apk
