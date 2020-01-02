f1229f352f6b962792ccb5ce288ac9f0ee9257f7
Alexey Min <alexey.min@gmail.com>
2020-01-02 16:50:51 +0300

    tests: openclose: optionally operate in fakeroot

--- tests/test_openclose.cpp
+++ tests/test_openclose.cpp
@@ -1,12 +1,29 @@
 #include <QCoreApplication>
+#include <QCommandLineOption>
+#include <QCommandLineParser>
 #include <QDebug>
+#include <QList>
 
 #include <QtApk.h>
 
-int main()
+int main(int argc, char *argv[])
 {
+    QCoreApplication app(argc, argv);
     int ret = 0;
     QtApk::Database db;
+    
+    QCommandLineOption root_option(
+        QStringLiteral("root"), QStringLiteral("Fake root dir path"),
+        QStringLiteral("root"));
+    
+    QCommandLineParser parser;
+    parser.addOption(root_option);
+    parser.addHelpOption();
+    parser.process(app);
+    
+    if (parser.isSet(root_option)) {
+        db.setUseFakeRoot(parser.value(root_option));
+    }
 
     if (!db.open(QtApk::Database::QTAPK_OPENF_READONLY)) {
         qWarning() << "Failed to open APK DB!";
